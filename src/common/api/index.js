a9f9c6277d088aedc1ecb470b46d923f7d7aa14b
Pavel Filonchyk <p_filonchyk@mail.ru>
2023-03-04 03:14:30 +0300

    добавил адрес сервера

--- src/common/api/index.js
+++ src/common/api/index.js
@@ -4,8 +4,8 @@ const {
     REACT_APP_SERVER_URL
 } = APP_CONFIG
 
-console.log(REACT_APP_SERVER_URL)
-export const POST_POKES = `http://localhost:8000/pokes`
+
+export const POST_POKES = `${REACT_APP_SERVER_URL}/pokes`
 
 export const POST_LOGIN = `${REACT_APP_SERVER_URL}/login`
 export const POST_REGISTRATION = `${REACT_APP_SERVER_URL}/registration`
